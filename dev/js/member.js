fd0c30859c4bf9f1c7f82f98bc32acd748d0227f
EricHong <f102041332@gmail.com>
2020-06-09 00:02:07 +0800

    member fix 1

--- dev/js/member.js
+++ dev/js/member.js
@@ -1,7 +1,19 @@
-
+let memData = {
+    memId : 10001,
+    memEmail : 'f1020@gmail.com',
+    memSurname : '黃',
+    memName : '阿炳',
+    memScore : 1020,
+    memImage : '',
+    memAddr : '桃園市中大路200號',
+    memPhone : '0922222222',
+    memHeight :178,
+    memWeight :65,
+    memGender : '男'
+}
 new Vue({
-    el: '#app',
+    el: '#memApp',
     data: {
-        message: 'Hello, VueJS!',
+        memData,
     },
 })
