7e5ec40b734346ca4b421876222cc377bea91aa3
EricHong <f102041332@gmail.com>
2020-06-15 22:25:53 +0800

    member救回來ㄌ

--- dev/js/member.js
+++ dev/js/member.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-let memData = {
-    memId : 10001,
-    memEmail : 'f1020@gmail.com',
-    memPsw : 'a1234567A',
-    memSurname : '黃',
-    memName : '阿炳',
-    memScore : 1020,
-    memImage : './images/member/rcc_author3.jpg',
-    memAddr : '桃園市中大路200號',
-    memPhone : '0922222222',
-    memHeight :178,
-    memWeight :65,
-    memGender : '男'
-=======
 window.addEventListener('load',gogoPower);
 
 function gogoPower(){
@@ -837,11 +822,7 @@ function gogoPower(){
             .go();
     }
 
->>>>>>> EricHong
 }
-new Vue({
-    el: '#memApp',
-    data: {
-        memData,
-    },
-})
+
+
+
