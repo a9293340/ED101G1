277b490516842744b175a8b25de53dc95c164a01
EricHong <f102041332@gmail.com>
2020-06-18 23:53:00 +0800

    member fix & 新增會員json & chart EXport.   20200618

--- dev/js/member.js
+++ dev/js/member.js
@@ -1,4 +1,5 @@
 import shopCart from '../../dev/js/layout/shoppingcart.js';
+import charLine from '../../dev/js/modules/chartLine.js';
 window.addEventListener('load',gogoPower);
 
 function gogoPower(){
@@ -63,7 +64,7 @@ function gogoPower(){
                 if(Number(changeCount) == 2){
                     // 生成折線圖
                     document.getElementById("myChart").innerHTML = '';
-                    memDrawChartLine();
+                    charLine(memHealthColdHot,memHealthHealth,memHealthStomach);
                 }
                 if(Number(changeCount) == 3){
                     let now = new Date();
@@ -528,107 +529,14 @@ function gogoPower(){
                     memHealthStomach.push(objStomach);
                 }
                 // 生成折線圖
-                document.getElementById("myChart").innerHTML = '';
-                memDrawChartLine(memHealthColdHot,memHealthHealth,memHealthStomach);
+                setTimeout(() => {
+                    document.getElementById("myChart").innerHTML = '';
+                    charLine(memHealthColdHot,memHealthHealth,memHealthStomach);
+                }, 100);
                 // console.log(this.memHealth)
             });
         },
     })
-
-    // chartjs
-    function memDrawChartLine(memHealthColdHot,memHealthHealth,memHealthStomach){
-        let ctx = document.getElementById("myChart").getContext('2d');
-        const colors = {
-            green: {
-                fill: '#e0eadf',
-                stroke: '#5eb84d',
-            },
-            lightBlue: {
-                stroke: '#6fccdd',
-            },
-            darkBlue: {
-                fill: '#92bed2',
-                stroke: '#3282bf',
-            },
-            purple: {
-                fill: '#8fa8c8',
-                stroke: '#75539e',
-            },
-        };
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            responsive: false,
-            data: {
-                datasets: [{
-                    label: 'ColdHot',
-                    fill: false,
-                    backgroundColor: '#ffffff',
-                    pointBackgroundColor: colors.purple.stroke,
-                    borderColor: colors.purple.stroke,
-                    pointHighlightStroke: colors.purple.stroke,
-                    borderCapStyle: 'butt',
-                    pointRadius:6,
-                    pointBackgroundColor: '#ffffff',
-                    pointBorderWidth: 1,
-                    data: memHealthColdHot,
-                },{
-                    label: 'Health',
-                    fill: false,
-                    backgroundColor: '#ffffff',
-                    pointBackgroundColor: colors.darkBlue.stroke,
-                    borderColor: colors.darkBlue.stroke,
-                    pointHighlightStroke: colors.darkBlue.stroke,
-                    borderCapStyle: 'butt',
-                    pointRadius:6,
-                    pointBackgroundColor: '#ffffff',
-                    pointBorderWidth: 1,
-                    data: memHealthHealth,
-                },{
-                    label: 'Stomach',
-                    fill: false,
-                    backgroundColor: '#ffffff',
-                    pointBackgroundColor: colors.lightBlue.stroke,
-                    borderColor: colors.lightBlue.stroke,
-                    pointHighlightStroke: colors.lightBlue.stroke,
-                    borderCapStyle: 'butt',
-                    pointRadius:6,
-                    pointBackgroundColor: '#ffffff',
-                    pointBorderWidth: 1,
-                    data: memHealthStomach,
-                }]
-            },
-            options: {
-                scales: {
-                    xAxes: [{
-                        type: "time",
-                        display: true,
-                        id: "date-axis",
-                        ticks: {
-                            source: "data",
-                            minRotation: 45,
-                        },
-                        gridLines: { 
-                            display: false
-                        } 
-                    }],
-                    yAxes: [{
-                        type: "linear",
-                        display: true,
-                        position: "left",
-                        ticks: {
-                            suggestedMin: 0,
-                            suggestedMax: 100,
-                            stepSize:20,
-                            beginAtZero: true,
-                        },
-                        gridLines: { 
-                            display: true
-                        } 
-                    }],
-                }
-            }
-        });
-    }
 
     //遊戲
     var gameObject = function(position,size,selector){
@@ -660,11 +568,8 @@ function gogoPower(){
         //繼承步驟1
         gameObject.call(this,this.position,this.size,'.ball');
     }
-        //繼承步驟2
     Ball.prototype = Object.create(gameObject.prototype);
-        //繼承步驟3
     Ball.prototype.constructor = Ball.constructor;
-    
     //球位置更新
     Ball.prototype.update = function(){
         this.position.x += this.velocity.x;
