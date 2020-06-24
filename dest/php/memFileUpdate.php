28d8eddc8ec645766b32ae501052423ce41409f0
EricHong <f102041332@gmail.com>
2020-06-24 23:18:27 +0800

    Ｏ＿Ｏ

---
+++ dest/php/memFileUpdate.php
@@ -0,0 +1,15 @@
+<?php 
+
+    session_start();
+    require_once("connect.php");
+    $sql2 = "update `member` set memName=:memName,memHeight=:memHeight,memWeight=:memWeight,memPhone=:memPhone,memAddr=:memAddr where memId=:memId";
+    $fixImage = $pdo->prepare($sql2);
+    $fixImage->bindValue(":memId",$_SESSION["memId"] );
+    $fixImage->bindValue(":memName",$_POST["memName"] );
+    $fixImage->bindValue(":memHeight",$_POST["memHeight"] );
+    $fixImage->bindValue(":memWeight",$_POST["memWeight"] );
+    $fixImage->bindValue(":memPhone",$_POST["memPhone"] );
+    $fixImage->bindValue(":memAddr",$_POST["memAddr"] );
+    $fixImage->execute();
+    echo "<script>alert('更新成功');location.href='../member.html';</script>";
+?>
