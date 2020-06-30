4d11f358d0d24093160b5de48b5ced62587fdbbe
jimmy5217123 <jimmy5217123@gmail.com>
2020-06-30 17:01:01 +0800

    shopping

--- dev/php/shopping.php
+++ dev/php/shopping.php
@@ -8,8 +8,13 @@ $memId = $totalOrder[3];
 $orderAdr = $totalOrder[4];
 $orderLorderListTextPost = $totalOrder[5];
 
-
-
+if(is_array($orderSin)){
+foreach($orderSin as $key => $value){
+    if( $key == "riceId"){
+        echo $value;
+    }
+}
+}
 // for( $i=0; $i<6; $i++){ 
 
 // echo($orderSin[0]['riceId']);
@@ -19,7 +24,10 @@ $orderLorderListTextPost = $totalOrder[5];
 //     echo $obj-> riceId;
 //     }
 // }
-var_dump($orderSin);
+// foreach($orderSin as $obj){
+//     echo $obj->riceId;
+// }
+// var_dump($orderSin);
 // require_once("connect.php");
 // $sqlSin = "SELECT * FROM `single_product`";
 
