e45214afa2cf7406da5795fae536f0dafe5eccfb
apple19890708 <apple19890708@gmail.com>
2020-07-02 16:54:05 +0800

    new_php

---
+++ dev/php/home_lBmessage.php
@@ -0,0 +1,9 @@
+<?php
+$lbMessage=[];
+require_once("connect.php");
+$sqlMessage= "SELECT mg.messContent as content,mg.messTime as mtime, m.memImage as img, m.memName as mname from `member` as m, `message` as mg  where mg.messBelong = m.memId and mg.messPostId = :mgPostId;";
+$messageSql = $pdo->prepare($$sqlMessage);
+$messageSql -> execute();
+$smessage = $messageSql->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($smessage);
+?>
--- dev/php/home_leaderBoard.php
+++ dev/php/home_leaderBoard.php
@@ -2,11 +2,11 @@
 
     $leaderBoards=[];
     require_once("connect.php");
-    $sqlsbenton = "SELECT s.soImg as img,m.memName as memname ,p.postDate as postdate,p.postId as bentonname,p.postTitle as title,p.postContent as content,p.postLike as `like` from `post` as p,`member` as m,`single_order` as s where p.postSoId = m.memId and s.soId = p.postId;";
+    $sqlsbenton = "SELECT s.soImg as img,m.memName as memname,m.memImage ,p.postDate as postdate,p.postId as bentonname,p.postTitle as title,p.postContent as content,p.postLike as `like` from `post` as p,`member` as m,`single_order` as s where p.postBelong = m.memId and s.soId = p.postSoId order by postLike desc limit 3;";
     $sbentonSql = $pdo->prepare($sqlsbenton);
     $sbentonSql -> execute();
-    $sbenton = $sbentonSql->fetchAll(PDO::FETCH_ASSOC);
+    $ssbenton = $sbentonSql->fetchAll(PDO::FETCH_ASSOC);
 
     // $leaderBoards=[$sbenton];
-    echo json_encode($sbenton);
+    echo json_encode($ssbenton);
 ?>
