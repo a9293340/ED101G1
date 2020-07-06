bad5e0e02e27ea5bccd2d74f7d31c42a6c27bb97
EricHong <f102041332@gmail.com>
2020-07-06 15:32:41 +0800

    1

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,11 +1,7 @@
 <?php
-	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
+	$dsn = "mysql:host=localhost;port=8889;dbname=ED101G1;charset=utf8";
 	$user = "root";
-<<<<<<< HEAD
-	$password = "qazwsx123";
-=======
-	$password = "may1234567";
->>>>>>> 187ffa65b8a2dda89bb77063b3b39261f1abef87
+	$password = "root";
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
