c3f634f98c078417fc99384db1f956fdb0c2773f
gn00558030 <gn00558030@gmail.com>
2020-07-07 00:29:46 +0800

    修改資料庫

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,12 +1,7 @@
 <?php
 	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
 	$user = "root";
-<<<<<<< HEAD
 	$password = "0422";
-=======
-	$password = "root";
->>>>>>> 7e03eabd3ac0cc3946552af4d350d3777f162302
->>>>>>> e171de22f12ca08783ee08200ae5daad89460f45
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
