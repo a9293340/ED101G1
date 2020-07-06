7e03eabd3ac0cc3946552af4d350d3777f162302
EricHong <f102041332@gmail.com>
2020-07-06 15:39:12 +0800

    health 校正

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,21 +1,8 @@
 <?php
-<<<<<<< HEAD
 	$dsn = "mysql:host=localhost;port=8888;dbname=ED101G1;charset=utf8";
 	$user = "root";
-<<<<<<< HEAD
-<<<<<<< HEAD
-	$password = "qazwsx123";
-=======
-	$password = "may1234567";
->>>>>>> 187ffa65b8a2dda89bb77063b3b39261f1abef87
-=======
 	$password = "root";
->>>>>>> tina
-=======
 	$dsn = "mysql:host=localhost;port=8889;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "root";
->>>>>>> EricHong
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
