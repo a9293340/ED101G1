28b3569b9835b4a6af3159a88865918f1669b173
apple19890708 <apple19890708@gmail.com>
2020-07-08 18:04:36 +0800

    new

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,8 +1,7 @@
 <?php
 	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
 	$user = "root";
-	$password = "root";
->>>>>>> 7e03eabd3ac0cc3946552af4d350d3777f162302
+	$password = "orange8477";
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
