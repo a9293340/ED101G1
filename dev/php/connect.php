52517252307d424c51a9476d35d25197acd64f9a
tinamin8110 <tinamin8110@gmail.com>
2020-07-12 21:42:48 +0800

    no message

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,23 +1,7 @@
 <?php
 	$dsn = "mysql:host=localhost;port=8888;dbname=ED101G1;charset=utf8";
 	$user = "root";
-<<<<<<< HEAD
 	$password = "root";
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	$password = "0422";
-=======
-	$password = "may1234567";
->>>>>>> ba09213c951fefe0a7003871f411fe9c23f1807a
-=======
-	$password = "root";
->>>>>>> tina
-=======
-	$password = "0422";
->>>>>>> show
->>>>>>> dev
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
