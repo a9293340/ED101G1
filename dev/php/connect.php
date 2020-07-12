a34a28b1cd5cace53682fd776a286370355e6e2a
Orange7877 <apple19890708@gmail.com>
2020-07-13 00:58:24 +0800

    animation

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,15 +1,8 @@
 <?php
 	$dsn = "mysql:host=localhost;port=8888;dbname=ED101G1;charset=utf8";
 	$user = "root";
-<<<<<<< HEAD
-<<<<<<< HEAD
-	$password = "0422";
-=======
-	$password = "may1234567";
->>>>>>> ba09213c951fefe0a7003871f411fe9c23f1807a
-=======
-	$password = "root";
->>>>>>> tina
+	$password = "qazwsx123";
+
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
