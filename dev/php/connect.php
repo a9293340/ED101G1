f3e176fd157f6a75151fddc97b571c165a80f74d
jimmy5217123 <jimmy5217123@gmail.com>
2020-07-15 23:20:53 +0800

    css

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,29 +1,7 @@
 <?php
-<<<<<<< HEAD
-    $dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "0422";
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
 	$user = "root";
-<<<<<<< HEAD
 	$password = "may1234567";
-=======
-	$password = "qazwsx123";
->>>>>>> orange
-=======
-	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "0422";
->>>>>>> show
-=======
-	$dsn = "mysql:host=localhost;port=8888;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "root";
->>>>>>> tina
->>>>>>> 8fe119f76394fbe82e5365c2bd0b8b0a40662071
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
