919da70339230d56c9e029b2d77600de304cc4c8
Orange7877 <apple19890708@gmail.com>
2020-07-18 22:09:11 +0800

    hot_fix

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,20 +1,6 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-	$dsn = "mysql:host=localhost;port=8888;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "root";
-=======
-
 	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
 	$user = "root";
-	$password = "may1234567";
->>>>>>> dev
-=======
-    $dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "0422";
-
->>>>>>> show
+	$password = "qazwsx123";
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
