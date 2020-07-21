a1766d9d3fe0027007796ebe6d1f904d510b5ebf
tinamin8110 <tinamin8110@gmail.com>
2020-07-21 14:45:08 +0800

    modify

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,15 +1,7 @@
 <?php
-<<<<<<< HEAD
-
-	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "may1234567";
-
-=======
 	$dsn = "mysql:host=localhost;port=8889;dbname=ED101G1;charset=utf8";
 	$user = "root";
 	$password = "root";
->>>>>>> tina
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
