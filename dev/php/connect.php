86c5f5bab6ece40acc57e909f78cff37a1952132
gn00558030 <gn00558030@gmail.com>
2020-07-21 17:02:29 +0800

    pic 更新上傳

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,38 +1,9 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-	$dsn = "mysql:host=localhost;port=8889;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "root";
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> dev
 
-
-	$dsn = "mysql:host=localhost;port=8889;dbname=ED101G1;charset=utf8";
+	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
 	$user = "root";
-<<<<<<< HEAD
-	$password = "root";
-=======
-	$password = "may1234567";
-<<<<<<< HEAD
->>>>>>> 33bdc58d75d33150b50c7417da45c575fbcfb72e
+	$password = "0422";
 
-=======
->>>>>>> show
-	$dsn = "mysql:host=localhost;port=8889;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "root";
->>>>>>> tina
-<<<<<<< HEAD
-=======
->>>>>>> 2740cfb16eabeafc4eec8ac7114374f3ee3bef83
->>>>>>> show
->>>>>>> fcc55ea4a85a4762f5c0c8bec3780ddb4f6284df
-=======
->>>>>>> dev
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
