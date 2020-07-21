3aa12a144e98cee3ade703844e8e53e7cc9b9961
gn00558030 <gn00558030@gmail.com>
2020-07-21 14:14:14 +0800

    調整新增便當資料庫名稱

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,45 +1,8 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	$dsn = "mysql:host=localhost;port=8888;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "root";
-=======
-
-	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "may1234567";
->>>>>>> dev
-=======
-=======
->>>>>>> show
     $dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
 	$user = "root";
 	$password = "0422";
 
-<<<<<<< HEAD
->>>>>>> show
-=======
-	$dsn = "mysql:host=localhost;port=8888;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "root";
->>>>>>> tina
-=======
-	$dsn = "mysql:host=localhost;port=3306;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "qazwsx123";
->>>>>>> orange
-=======
-	$dsn = "mysql:host=localhost;port=8888;dbname=ED101G1;charset=utf8";
-	$user = "root";
-	$password = "root";
->>>>>>> tina
-=======
->>>>>>> show
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options); 
 ?>
